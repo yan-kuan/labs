ceea6c958b34163dc9154e3afc84e303492aff16
yenWu <yenwu.tw@gmail.com>
2020-01-07 16:47:56 +0800

    wget vs axel: comparasion of traditional and parallel downloader
    axel - a download accelerator. We specified maximum cores for
    	parallel download.

---
+++ wget-vs-axel/axel-cmd.sh
@@ -0,0 +1,1 @@
+axel --num-connections=`nproc` https://cdimage.debian.org/debian-cd/current/amd64/iso-dvd/debian-10.2.0-amd64-DVD-2.iso 2>&1 | tee axel.log
---
+++ wget-vs-axel/wget-cmd.sh
@@ -0,0 +1,1 @@
+wget https://cdimage.debian.org/debian-cd/current/amd64/iso-dvd/debian-10.2.0-amd64-DVD-2.iso 2>&1 | tee wget.log
